b07821ed0bc6c9d4a39ccee4852636d187224142
vivianngozi <orjingozy@yahoo.com>
2024-01-24 14:17:09 +0100

    now

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default {
     connect() {
-        mongoose.connect('mongodb://localhost/project');
+        mongoose.connect('mongodb://100.20.92.101/project');
         const db = mongoose.connection;
         db.on("error", console.error.bind(console, "connection error: "));
         db.once("open", function() {
