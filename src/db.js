1f58bc7808a111594de6f2d28ce0a51849aa3d29
vivianngozi <orjingozy@yahoo.com>
2024-01-31 13:21:19 +0100

    chance

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
-import MongoClient from "mongoose";
+import mongoose from "mongoose";
 
 export default {
     connect() {
         // let url = 'mongodb+srv://orjiviviana:chinyere2000@cluster0.mhsh0ku.mongodb.net/?retryWrites=true&w=majority';
-        MongoClient.connect('mongodb://localhost/project');
-        const db = MongoClient.connection;
+        mongoose.connect('mongodb+srv://orjiviviana:chinyere2000@cluster0.yv4tczf.mongodb.net/?retryWrites=true&w=majority');
+        const db = mongoose.connection;
         db.on("error", console.error.bind(console, "connection error: "));
         db.once("open", function() {
             console.log("Connected Successfully");
