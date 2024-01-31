829bc789d8b90375248ccf78fcc30260392eed56
vivianngozi <orjingozy@yahoo.com>
2024-01-31 13:29:43 +0100

    new

--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export default {
     connect() {
-        // let url = 'mongodb+srv://orjiviviana:chinyere2000@cluster0.mhsh0ku.mongodb.net/?retryWrites=true&w=majority';
-        mongoose.connect('mongodb+srv://orjiviviana:chinyere2000@cluster0.yv4tczf.mongodb.net/?retryWrites=true&w=majority');
+        
+        mongoose.connect(process.env.MONGO_URL);
         const db = mongoose.connection;
         db.on("error", console.error.bind(console, "connection error: "));
         db.once("open", function() {
